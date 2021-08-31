62bdaf26605cdb2bc30c5d889ab36cebbb7e7940
Prabhat Malhan <prabhatmalhan.cse@geu.ac.in>
2021-08-31 13:11:11 +0530

    info configuration updater

---
+++ Data Collection/resetResource.py
@@ -0,0 +1,9 @@
+from EditJson import EditConfig
+
+
+def resetConfig():
+    p = int(input('enter user id : '))
+    for i in range(22):
+        x = EditConfig('./Resources/'+str(i)+'/info.json').readConfig()
+        x['count'] = str(p*10)
+        EditConfig('./Resources/'+str(i)+'/info.json').writeConfig(x)
