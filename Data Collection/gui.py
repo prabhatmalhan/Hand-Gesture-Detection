c6400142e6616b88d308e6076ed74fcb57d29df8
Prabhat Malhan <prabhatmalhan.cse@geu.ac.in>
2021-08-31 13:11:44 +0530

    gui created

---
+++ Data Collection/gui.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from ResourceCollection import *
+from resetResource import resetConfig
+
+
+def buttonClick(i):
+    global x
+    if capture(str(i)):
+        x[i]['state'] = DISABLED
+        x[i]['text'] = 'done'
+
+
+resetConfig()
+root = Tk()
+root.geometry("200x700+1300+50")
+n = ['Peace', 'You', 'Good Job', 'Dislike', 'Ok', 'Good Luck', 'Rock', 'Bang', 'Shocker', 'Bump',
+     'Help', '1', '2', '3', '4', '5', 'Sprinkle', 'Half Heart', 'Yum', 'Love', 'Naruto', 'Kaneki']
+x = list()
+for j, i in enumerate(n):
+    x.append(Button(root, text=i, width=15,
+                    command=lambda c=j: buttonClick(c)))
+    x[j].place(x=45, y=10+j*30)
+root.mainloop()
