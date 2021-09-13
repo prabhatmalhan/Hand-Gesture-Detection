ab47767e874c68b740c5df1ca773d66b99792c4c
prabhatmalhan <prabhatmalhan.cse@geu.ac.in>
2021-09-13 07:10:01 +0530

    resource path and count update

--- Data Collection/resetResource.py
+++ Data Collection/resetResource.py
@@ -4,6 +4,6 @@ from EditJson import EditConfig
 def resetConfig():
     p = int(input('enter user id : '))
     for i in range(22):
-        x = EditConfig('./Resources/'+str(i)+'/info.json').readConfig()
-        x['count'] = str(p*240)
-        EditConfig('./Resources/'+str(i)+'/info.json').writeConfig(x)
+        x = EditConfig('..\\Resources\\'+str(i)+'\\info.json').readConfig()
+        x['count'] = str(p*60)
+        EditConfig('..\\Resources\\'+str(i)+'\\info.json').writeConfig(x)
